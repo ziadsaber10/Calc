bb716ea519c13b111ceae65e654f831316f7df84
ziadsaber10 <ziadsaber2003@gmail.com>
2025-03-20 23:11:14 +0200

    ITI

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function append(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    let expression = document.getElementById('display').value;
+    try {
+        let result = eval(expression);
+        document.getElementById('display').value = result;
+    } catch (e) {
+        document.getElementById('display').value = "Error";
+    }
+}
